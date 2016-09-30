2a5007d8d0a7b20e4afb3b26024878b09fc84a72
eric.sindelar <eric.sindelar@insureon.com>
2016-09-30 16:35:49 -0500

    single file OWIN/Web API application.

---
+++ WebApi2-OWIN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace ersOWIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApp.Start<Config>("http://localhost:8099");
+            Thread.Sleep(-1);
+        }
+    }
+
+    public class TimeController : ApiController
+    {
+        [HttpGet]
+        [Route("time")]
+        public string WhatTime() => $"hello, it's {DateTime.Now}";
+    }
+
+    public class Config
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+        }
+    }
+}
